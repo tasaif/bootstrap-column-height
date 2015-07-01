b3d82bcae34e955f817133dcdd7450f5f20fade6
Derek <saif.tareq@gmail.com>
2015-06-30 17:00:40 -0700

    Initially match heights before binding to changes in dom

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-Math.sqrt(5)
-parseInt(2.1234)
-// Single line
+$(function(){
+  (function(){
+    function getColumns($row){
+      var col_classes = ['xs', 'sm', 'md', 'lg'];
+      var $columns = $();
+      for(var col_class_i=0; col_class_i<col_classes.length; col_class_i++){
+        for(var cell_size_i=1; cell_size_i<=12; cell_size_i++){
+          var column_class = 'col-' + col_classes[col_class_i] + '-' + cell_size_i;
+          $.merge($columns, $row.find('> .' + column_class));
+        }
+      }
+      return $columns;
+    }
+    function matchHeights($columns){
+      var largest_height = 0;
+      $columns.each(function(){
+        var height = $(this).height();
+        if (height > largest_height) largest_height = height;
+      });
+      $columns.each(function(){
+        $(this).height(largest_height);
+      });
+    }
 
-/*
-* Multi line
-*/
-
-for (var i=0; i<100; i++){
-  if (i%3 == 0){
-    console.log(i);
-  }
-}
+    $('[data-match-columns="true"]').each(function(){
+      var $row = $(this);
+      $columns = getColumns($row);
+      matchHeights($columns);
+    });
+  })();
+});
